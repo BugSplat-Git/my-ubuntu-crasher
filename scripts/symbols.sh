8f97271cc6025215f0ab94cc18a790f892bbda5a
Bobby Galli <bobbyg603@gmail.com>
2020-08-05 12:28:16 -0400

    Renamed sym_upload to symupload

--- scripts/symbols.sh
+++ scripts/symbols.sh
@@ -3,4 +3,4 @@ source exports.sh
 export SYM_FILE=$OUT_DIR/$MODULE_NAME.sym
 
 $CRASHPAD_DIR/tools/dump_syms $PROJECT_DIR/out/$MODULE_NAME > $SYM_FILE
-$CRASHPAD_DIR/tools/sym_upload $SYM_FILE "https://$BUGSPLAT_DATABASE.bugsplat.com/post/bp/symbol/breakpadsymbols.php?appName=$BUGSPLAT_APP_NAME&appVer=$BUGSPLAT_APP_VERSION"
+$CRASHPAD_DIR/tools/symupload $SYM_FILE "https://$BUGSPLAT_DATABASE.bugsplat.com/post/bp/symbol/breakpadsymbols.php?appName=$BUGSPLAT_APP_NAME&appVer=$BUGSPLAT_APP_VERSION"
