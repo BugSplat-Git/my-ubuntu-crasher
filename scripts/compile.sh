7e76bfa200951ecb131372d593ddcdec94c68034
Bobby Galli <bobbyg603@gmail.com>
2022-03-18 08:29:10 -0400

    Add client/libcommon.a

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -2,11 +2,13 @@
 source exports.sh
 
 clang++ -pthread $PROJECT_DIR/main.cpp \
+    $CRASHPAD_DIR/lib/libcommon.a \
     $CRASHPAD_DIR/lib/libclient.a \
     $CRASHPAD_DIR/lib/libutil.a \
     $CRASHPAD_DIR/lib/libbase.a \
     -I$CRASHPAD_DIR/include \
     -I$CRASHPAD_DIR/include/third_party/mini_chromium/mini_chromium \
+    -I$CRASHPAD_DIR/include/out/Default/gen \
     -o$OUT_DIR/$MODULE_NAME \
     -g \
     -Wl,--build-id
--- scripts/crashpad.sh
+++ scripts/crashpad.sh
@@ -5,6 +5,7 @@ source exports.sh
 cp $CRASHPAD_BUILD_DIR/crashpad_handler $CRASHPAD_DIR/bin
 
 # Copy lib
+cp $CRASHPAD_BUILD_DIR/obj/client/libcommon.a $CRASHPAD_DIR/lib/libcommon.a
 cp $CRASHPAD_BUILD_DIR/obj/client/libclient.a $CRASHPAD_DIR/lib/libclient.a
 cp $CRASHPAD_BUILD_DIR/obj/util/libutil.a $CRASHPAD_DIR/lib/libutil.a
 cp $CRASHPAD_BUILD_DIR/obj/third_party/mini_chromium/mini_chromium/base/libbase.a $CRASHPAD_DIR/lib/libbase.a
