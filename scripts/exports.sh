f6e37cb56a47c3760eeb29e042ee048c225cd053
Bobby Galli <bobbyg603@pm.me>
2024-07-05 15:39:29 -0400

    chore: replace dump_syms and symupload with @bugsplat/symbol-upload

--- scripts/exports.sh
+++ scripts/exports.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 export CRASHPAD_CHECKOUT_DIR=${CRASHPAD_CHECKOUT_DIR:-"$HOME/Desktop/github/crashpad"}
 export CRASHPAD_BUILD_DIR=${CRASHPAD_BUILD_DIR:-"$CRASHPAD_CHECKOUT_DIR/out/Default"}
-export PROJECT_DIR=${PROJECT_DIR:-"$HOME/Desktop/github/my-ubuntu-crasher"}
+export PROJECT_DIR=${PROJECT_DIR:-"/root/github/my-ubuntu-crasher/"}
 export CRASHPAD_DIR=${CRASHPAD_DIR:-"$PROJECT_DIR/crashpad"}
 export OUT_DIR=${OUT_DIR:-"$PROJECT_DIR/out"}
 export MODULE_NAME=${MODULE_NAME:-"myUbuntuCrasher"}
+export BUGSPLAT_EMAIL=${BUGSPLAT_EMAIL:-"fred@bugsplat.com"}
+export BUGSPLAT_PASSWORD=${BUGSPLAT_PASSWORD:-"Flintstone"}
 export BUGSPLAT_DATABASE=${BUGSPLAT_DATABASE:-"fred"}
 export BUGSPLAT_APP_NAME=${BUGSPLAT_APP_NAME:-"myUbuntuCrasher"}
 export BUGSPLAT_APP_VERSION=${BUGSPLAT_APP_VERSION:-"1.0.0"}
