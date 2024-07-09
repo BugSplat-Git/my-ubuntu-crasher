be3f36ad28d02845d6f996c2bb32c971928c829e
Bobby Galli <bobbyg603@gmail.com>
2024-07-09 08:19:22 -0400

    chore: replace dump_syms and symupload with @bugsplat/symbol-upload (#8)
    * chore: replace dump_syms and symupload with @bugsplat/symbol-upload
    
    * chore: update README.md

--- scripts/symbols.sh
+++ scripts/symbols.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 source exports.sh
-export SYM_FILE=$OUT_DIR/$MODULE_NAME.sym
 
-$CRASHPAD_DIR/tools/dump_syms $PROJECT_DIR/out/$MODULE_NAME > $SYM_FILE
-$CRASHPAD_DIR/tools/sym_upload $SYM_FILE "https://$BUGSPLAT_DATABASE.bugsplat.com/post/bp/symbol/breakpadsymbols.php?appName=$BUGSPLAT_APP_NAME&appVer=$BUGSPLAT_APP_VERSION"
+$CRASHPAD_DIR/tools/symbol-upload-linux -b $BUGSPLAT_DATABASE \
+    -a $BUGSPLAT_APP_NAME \
+    -v $BUGSPLAT_APP_VERSION \
+    -u $BUGSPLAT_EMAIL \
+    -p $BUGSPLAT_PASSWORD \
+    -d $PROJECT_DIR/out \
+    -f $MODULE_NAME \
+    --dumpSyms
+
