79a5df46b185186cc5dc19578f9025d2d4043ba0
Bobby Galli <bobbyg603@gmail.com>
2025-05-19 20:01:28 -0400

    chore: fix exports.sh

--- scripts/exports.sh
+++ scripts/exports.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export CRASHPAD_CHECKOUT_DIR=${CRASHPAD_CHECKOUT_DIR:-"$HOME/Desktop/github/crashpad"}
 export CRASHPAD_BUILD_DIR=${CRASHPAD_BUILD_DIR:-"$CRASHPAD_CHECKOUT_DIR/out/Default"}
-export PROJECT_DIR=${PROJECT_DIR:-"/mnt/c/Users/BobbyGalli/Desktop/GitHub/my-ubuntu-crasher"}
+export PROJECT_DIR=${PROJECT_DIR:-"/root/github/my-ubuntu-crasher/"}
 export CRASHPAD_DIR=${CRASHPAD_DIR:-"$PROJECT_DIR/crashpad"}
 export OUT_DIR=${OUT_DIR:-"$PROJECT_DIR/out"}
 export MODULE_NAME=${MODULE_NAME:-"myUbuntuCrasher"}
